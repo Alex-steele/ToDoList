0d92ad96a22f27c3a8900811fab08da49720cd92
Alex Steele <alex.steele@bud.co.uk>
2020-12-14 12:18:55 +0000

    Add basic working version with ability to add items to list

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace ToDoList
+namespace ToDoList.Core
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); 
+            Console.WriteLine("Type a message to add to your To-Do List");
+
+            var listItems = new List<string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                if (input == "d")
+                {
+                    Console.WriteLine("\nTo-Do List");
+                    Console.WriteLine("----------");
+                    foreach (var item in listItems)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                else
+                {
+                    listItems.Add(input);
+                }
+
+                Console.WriteLine("\nAdd another item or type d to display the list or type q to quit");
+            }
         }
     }
 }
