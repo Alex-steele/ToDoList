95715e92a15df85ea5a834cfb7a7f39e6c7e91c7
Alex Steele <alex.steele@bud.co.uk>
2020-12-15 14:08:36 +0000

    oops I forgot to actually add the test files to that last commit

---
+++ ToDoList.Tests/AddCommandTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using ToDoList.Core;
+using ToDoList.Core.Commands;
+
+namespace ToDoList.Tests
+{
+    [TestFixture]
+    public class AddCommandTests
+    {
+        [Test]
+        public void AddItem_ListItemsIsNull_ThrowsException()
+        {
+            // Arrange
+            var listItems = (List<ListItem>) null;
+            const string itemValue = "Test Value";
+
+            // Act & Assert
+            Assert.That(() => AddCommand.AddItem(listItems, itemValue), Throws.Exception);
+        }
+
+        [Test]
+        public void AddItem_ListItemsIsEmpty_AddsItemWithId1()
+        {
+            // Arrange
+            var listItems = new List<ListItem>();
+            const string itemValue = "Test Value";
+
+            // Act
+            AddCommand.AddItem(listItems, itemValue);
+
+            // Assert
+            Assert.That(listItems.Single().Id, Is.EqualTo(1));
+            Assert.That(listItems.Single().Value, Is.EqualTo("Test Value"));
+        }
+
+        [Test]
+        public void AddItem_ListItemsIsNotEmpty_AddsItemWithNextId()
+        {
+            // Arrange
+            var listItems = new List<ListItem>
+            {
+                new ListItem
+                {
+                    Id = 1
+                }
+            };
+
+            const string itemValue = "Test Value";
+
+            // Act
+            AddCommand.AddItem(listItems, itemValue);
+
+            // Assert
+            Assert.That(listItems.Last().Id, Is.EqualTo(2));
+            Assert.That(listItems.Last().Value, Is.EqualTo("Test Value"));
+        }
+    }
+}
---
+++ ToDoList.Tests/CompleteCommandTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using ToDoList.Core;
+using ToDoList.Core.Commands;
+
+namespace ToDoList.Tests
+{
+    [TestFixture]
+    public class CompleteCommandTests
+    {
+        [Test]
+        public void CompleteItem_InputIdMatchesItemId_ItemIsCompleted()
+        {
+            // Arrange
+            var listItems = new List<ListItem>
+            {
+                new ListItem
+                {
+                    Id = 1,
+                    Completed = false
+                }
+            };
+
+            const int id = 1;
+
+            // Act
+            CompleteCommand.CompleteItem(listItems, id);
+
+            // Assert
+            Assert.IsTrue(listItems.Single().Completed);
+        }
+
+        [Test]
+        public void CompleteItem_InputIdDoesNotMatchItemId_ThrowsException()
+        {
+            // Arrange
+            var listItems = new List<ListItem>
+            {
+                new ListItem
+                {
+                    Id = 1,
+                    Completed = false
+                }
+            };
+
+            const int id = 2;
+
+            // Act & Assert
+            Assert.That(() => CompleteCommand.CompleteItem(listItems, id), Throws.Exception);
+        }
+
+        [Test]
+        public void CompleteItem_ListItemsIsNull_ThrowsException()
+        {
+            // Arrange
+            var listItems = (List<ListItem>) null;
+            const int id = 1;
+
+            // Act & Assert
+            Assert.That(() => CompleteCommand.CompleteItem(listItems, id), Throws.Exception);
+        }
+
+        [Test]
+        public void CompleteItem_ListItemsIsEmpty_ThrowsException()
+        {
+            // Arrange
+            var listItems = new List<ListItem>();
+            const int id = 1;
+
+            // Act & Assert
+            Assert.That(() => CompleteCommand.CompleteItem(listItems, id), Throws.Exception);
+        }
+    }
+}
